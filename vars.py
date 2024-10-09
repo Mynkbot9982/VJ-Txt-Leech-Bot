1b69b1a8748c219e156a9898babea70084a04598
Gkkumar99 <kumarrmynk@gmail.com>
2024-10-09 19:41:35 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,15 @@
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
 from os import environ
 
+API = environ.get("API", "") # shortlink api
+URL = environ.get("URL", "") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
+BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
+VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
+
+# OWNER information 
 API_ID = int(environ.get("API_ID", "23917270"))
 API_HASH = environ.get("API_HASH", "596a4c1d72ba6d00056f8733f6f3e5fb")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
