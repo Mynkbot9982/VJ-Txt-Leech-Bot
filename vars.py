a85baa139e49d439e68561596eb2425469446ac4
Gkkumar99 <kumarrmynk@gmail.com>
2024-10-10 10:53:50 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -10,6 +10,7 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "") # bot username without @
 VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
 
 # OWNER information 
+
 API_ID = int(environ.get("API_ID", "23917270"))
 API_HASH = environ.get("API_HASH", "596a4c1d72ba6d00056f8733f6f3e5fb")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
